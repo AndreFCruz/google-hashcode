fced9f71a821fa70c132bfe4a52a1bfc42c4ecee
AndreFCruz <andrecruz97@gmail.com>
2017-02-07 15:24:01 +0000

    First version of solution.

--- pizza.cpp
+++ pizza.cpp
@@ -8,24 +8,34 @@
 
 #include <iostream>
 #include <vector>
-#include <set>
 #include <fstream>
 
 using namespace std;
 
 typedef unsigned int uint;
 
-// Globals
-bool visited[1000][1000];
+
 
 /* Fins all pairs of numbers whose product is n
  */
-set<pair<uint, uint>> find_products(uint n) {
-    set<pair<uint, uint>> result;
+vector<pair<uint, uint>> find_products(uint n) {
+    vector<pair<uint, uint>> result;
     
-    for (uint k = 1; k * k <= n; ++k) {
-        if (k * (n / k) == n)   // integer division resulted in correct pair?
-            result.insert(make_pair(k, n / k));
+    for (uint k = 1; k <= n; ++k) {
+        if (k * (n / k) == n) { // integer division resulted in correct pair?
+            result.push_back(make_pair(k, n / k));
+        }
+    }
+    
+    return result;
+}
+
+vector<pair<uint, uint>> find_all_products(uint n) {
+    vector<pair<uint,uint>> result;
+    
+    for (uint i = n; i > 1; --i) {
+        auto tmp = find_products(i);
+        result.insert(end(result), begin(tmp), end(tmp));
     }
     
     return result;
@@ -38,15 +48,7 @@ private:
     
 public:
     Slice(uint row_i, uint row_f, uint col_i, uint col_f) : rows(make_pair(row_i, row_f)), cols(make_pair(col_i, col_f)) {
-        cerr << "New Slice: " << row_i << row_f << col_i << col_f << endl;
-    }
-    
-    void markVisited() const {
-        for (uint r = rows.first; r <= rows.second; ++r) {
-            for (uint c = cols.first; c <= cols.second; ++c) {
-                visited[r][c] = true;
-            }
-        }
+        cerr << "New Slice: " << row_i << " " << row_f << " " << col_i << " " << col_f << endl;
     }
     
     friend ostream & operator<<(ostream & out, const Slice & s) {
@@ -57,23 +59,55 @@ public:
 
 class Pizza {
 private:
-	uint rows;
-	uint cols;
-	uint min_ing;	// minimum number of ingredients per slice
-	uint max_cells;	// maximum number of cells per slice
-	vector<string> content;	// Matrix of ingredients
+    uint rows;
+    uint cols;
+    uint min_ing;	// minimum number of ingredients per slice
+    uint max_cells;	// maximum number of cells per slice
+    vector<string> contents;	// Matrix of ingredients
     vector<Slice> slices;
+    vector< vector<bool> > visited;
+    
+    void markVisited(uint ri, uint rf, uint ci, uint cf) {
+        for (uint row = ri; row <= rf; ++row) {
+            for (uint col = ci; col <= cf; ++col) {
+                visited[row][col] = true;
+            }
+        }
+    }
+    
+    bool isValid(uint ri, uint rf, uint ci, uint cf) const {
+        if (ri > rf || ci > cf || rf >= rows || cf >= cols)
+            return false;
+        
+        bool tomatoes = false, mushrooms = false;
+        
+        for (uint row = ri; row <= rf; ++row) {
+            for (uint col = ci; col <= cf; ++col) {
+                if (visited[row][col]) {
+                    return false;
+                }
+                if (contents[row][col] == 'T')
+                    tomatoes = true;
+                else if (contents[row][col] == 'M')
+                    mushrooms = true;
+            }
+        }
+        
+        return (tomatoes && mushrooms);
+    }
     
 public:
-	Pizza(ifstream & ifs) {
+    Pizza(ifstream & ifs) {
         ifs >> rows >> cols >> min_ing >> max_cells;
         ifs.ignore(3, '\n');
         
         string line;
         for (uint i = 0; i < rows; ++i) {
             getline(ifs, line);
-            content.push_back(line);
+            contents.push_back(line);
         }
+        
+        visited = vector<vector<bool>>(rows, vector<bool>(cols, false));
     }
     
     friend ostream & operator<<(ostream & out, const Pizza & p) {
@@ -92,11 +126,8 @@ public:
      */
     void divide() {
         slices.clear();
-        auto sizes = find_products(max_cells);
+        auto sizes = find_all_products(max_cells);
         bool finished = false;
-        
-        // example choice -- to be tuned
-        auto choice = *(sizes.begin());
         
         uint row = 0, col = 0;
         while (! finished) {
@@ -108,16 +139,27 @@ public:
                     col = 0;
                 } else {
                     finished = true;
+                    break;
                 }
             }
             
-            slices.emplace_back(row, row + choice.first, col, col + choice.second);
+            for (auto choice : sizes) {
+                if (! isValid(row, row + choice.first - 1, col, col + choice.second - 1))
+                    continue;
+                
+                slices.emplace_back(row, row + choice.first - 1, col, col + choice.second - 1);
+                markVisited(row, row + choice.first - 1, col, col + choice.second - 1);
+                
+                /* for optimization purposes
+                 row += choice.first - 1;
+                 col += choice.second - 1;
+                 */
+            }
             
-            slices.back().markVisited();
-            
+            if (! (row < rows && col < cols))
+                finished = true;
         }
         
-        cerr << "Score (more is worse): ";
         uint score = 0;
         for (uint r = 0; r < rows; ++r) {
             for (uint c = 0; c < cols; ++c) {
@@ -125,22 +167,22 @@ public:
                     score++;
             }
         }
-        cerr << score << endl;
+        cerr << "**SCORE** " << score << endl;
     }
 };
 
 
 
 int main ( int argc, char * argv[] ) {
-	if (argc != 1 + 2) {
-		cout << "Incorrect number of arguments. Usage: ./pizza input_file_path output_file" << endl;
-		return 0;
-	}
-
-	string input (argv[1]);
-	string output (argv[2]);
-
-	ifstream ifs (input);
+    if (argc != 1 + 2) {
+        cout << "Incorrect number of arguments. Usage: ./pizza input_file_path output_file" << endl;
+        return 0;
+    }
+    
+    string input (argv[1]);
+    string output (argv[2]);
+    
+    ifstream ifs (input);
     if (! ifs.is_open()) {
         cout << "Input file not found! Was \"" << input << "\"" << endl;
         return 0;
@@ -153,7 +195,7 @@ int main ( int argc, char * argv[] ) {
     // TODO: change to file ostream
     cout << pizza;
     
-
-
-	return 0;
+    
+    
+    return 0;
 }
